5327c981c67619e5f0f4a62916a74874c112d0d2
Techjulie <168008144+babyjulie@users.noreply.github.com>
2024-09-22 08:40:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Julie's Orphanage Homes &mdash; Home Page</title>
+
+  <?php include('meta.php'); ?>
+
+</head>
+
+<body>
+  <!-- header -->
+
+
+
+  <!--nav bar starts-->
+
+  <?php include('header.php'); ?>
+  <!--nav bar ends-->
+
+  <!--carousel starts-->
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="h-100 w-100" src="caro2.jpg" class="d-block w-100" alt="orphan" />
+        <div class="carousel-caption d-md-block">
+          <h2><span>Juli's Orphanage </span>Homes</h2>
+          <p>Where Children Find Love and Support....Hope and Love Resides here</p>
+          <a href="donate.php" type="button" class="btn text-white" style="background: rgb(183, 0, 31);">Donate now</button></a>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="h-100 w-100" src="bg.jpg" class="d-block w-100" alt="orphan" />
+        <div class="carousel-caption d-md-block">
+          <h2><span>Juli's Orphanage </span>Homes</h2>
+          <p>Where Children Find Love and Support....Hope and Love Resides here</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="h-100 w-100" src="chi5.jpg" class="d-block w-100" alt="orphan" />
+        <div class="carousel-caption d-md-block">
+          <h2><span>Juli's Orphanage </span>Homes</h2>
+          <p>
+            Where Children Find Love and Support....Hope and Love Resides here
+          </p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+  <div class="row mt-5" style="background-color: rgba(155, 4, 4, 0.2); ">
+    <div class="col-md-6">
+      <div class="container">
+        <img src="caro3.jpg" alt="" style="height: 360px" />
+      </div>
+    </div>
+    <div class="col-md-6" style="align-items: center;">
+      <div>
+        <p>WELCOME TO JULIES ORPHANAGE HOME</p>
+        <h1 style="color: rgb(183, 0, 31);">Make your goals to help children</h1>
+        <p class="mt-3">
+          JULIES ORPHANAGE advocates and works to give children in
+          institutions and unplanned newborn babies the opportunity to grow in
+          the care of a family. Make a difference with your generosity.
+          OrphanCare works to ensure at-risk children grow up in safe, loving
+          environments. Donations
+          can be made with a debit or credit card.
+        </p>
+      </div>
+      <div class="row mt-4">
+        <div class="col"><img src="care.png" alt=" " height="80px"></div>
+        <div class="col">
+          <p>Let’s sponsor an entire project</p>
+        </div>
+        <div class="col"><img src="give.png" alt=" " height="80px"></div>
+        <div class="col">
+          <p>Donate to the new cause</p>
+        </div>
+      </div>
+      <div><img src="check-icon.png" alt="" height="10px"> Your generosity will make a significant difference in the lives of our children.
+        <p><img src="check-icon.png" alt="" height="10px"> Thank you for your kindness and support!</p>
+      </div>
+      <a href="donate.php" type="button" class="btn text-white" style="background: rgb(183, 0, 31);">Donate now</button></a>
+
+    </div>
+  </div>
+  <div class="container-fluid mt-5 p-5 mb-5 h-100" style="background-color: rgba(155, 4, 4, 0.2); ">
+    <div class="row mt-5 mb-3">
+      <div class="col-md-3">
+        <p>Change everything</p>
+        <h4 class="h1 mb-4" style="color: rgb(183, 0, 31);">We’re here to help them</h4>
+        <p class="text-muted">
+          <strong>At Julie'sOrphanage, </strong> believe that every child deserves access to nutritious food to grow and thrive
+          <strong>education</strong> is the key to unlocking a brighter future for our children.
+          Give a <strong>Loving</strong> Family to a Child in Need
+          we believe that every child deserves a loving family and a forever home.
+
+
+
+        </p>
+      </div>
+
+      <div class="col-md-3 mb-5">
+        <div class="card" style="width: 18rem; text-align: center">
+          <div class="card-body">
+            <h5 class="card-title" style="color: rgb(183, 0, 31);">HEALTHY FOOD</h5>
+
+            <p class="card-text text-muted">
+
+              Many of our children suffer from malnutrition, weakened immune systems, and impaired cognitive development.
+              Healthy food is essential for our children to focus in school, play safely.
+              Your support will help us provide balanced meals, fresh fruits and vegetables, and healthy snacks to our children.
+
+              <br />
+              <img class="mb-5 mt-5" src="food1.png" alt="" height="75px" ; width="160px" />
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-5">
+        <div class="card" style="width: 18rem; text-align: center">
+          <div class="card-body">
+            <h5 class="card-title" style="color: rgb(183, 0, 31);">KIDS EDUCATION</h5>
+
+            <p class="card-text text-muted">
+
+              Education helps our children develop critical thinking, problem-solving, and communication skills, essential for success in life.
+              Education opens doors to better job opportunities, improved socio-economic status.
+              Education fosters creativity driving progress and growth in our communities.
+              <br />
+              <img class="mb-5 mt-5" src="graduate.png" alt="" height="75px" ; width="160px" />
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-5" style="">
+        <div class="card" style="width: 18rem; text-align: center">
+          <div class="card-body">
+            <h5 class="card-title" style="color: rgb(183, 0, 31);">ADOPTION</h5>
+
+            <p class="card-text text-muted">
+              Eligibility: We work with singles, couples, and families.
+              Adoption process involves application, home study, matching, and post-placement support.<br>
+              We charge a reasonable adoption fee to cover legal and administrative costs.
+            <p><img class="mb-5 mt-5" src="adopt.png" alt="" height="75px" ; width="160px" ; />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--card ends here-->
+
+  <div class="row mt-5 pt-5" style="
+        width: 100%;
+        text-align: center;
+        height: 600px;
+        background: url(happy.jpg);
+    background-color: #333;
+    opacity: .8;
+    background-blend-mode: multiply;
+      ">
+    <div class="col-md-12 m-5 p-5" style="color: white;">
+      <strong>We’re here to support orphans</strong>
+      <h1 style="font-size: 50px; color:white ">
+        Fundraising for the children <br />
+        you care about
+      </h1>
+      <a href="donate.php" type="button" class="btn text-white" style="background: rgb(183, 0, 31);">Donate now</button></a>
+
+    </div>
+  </div>
+  <!--fundraising stops-->
+
+  <div class="container mt-5">
+    <p>Start donating them</p>
+    <h1 style="color: rgb(183, 0, 31);">Find popular causes</h1>
+  </div>
+
+  <!--3 cards-->
+  <div class="container mt-2 p-5 mb-5 h-100" style="background-color: rgba(155, 4, 4, 0.2); ">
+    <div class="row mt-5 mb-5">
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" src="caro3.jpg" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title" style="color: rgb(183, 0, 31);"> <a href="">Sponsor a child's skills training</a></h5>
+            <p class="card-text text-muted">
+
+              We believe that every child deserves the opportunity to reach their full potential.
+              Our skills training programs include:
+              Vocational training (carpentry, tailoring, cooking)
+              Computer literacy and coding
+            </p>
+            <hr>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" src="caro1.jpg" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title" style="color: rgb(183, 0, 31);"><a href="">Sponsor a Child's education </a></h5>
+            <p class="card-text text-muted">
+              Your support will help us provide our children with the education they deserve,
+              empowering them to reach their full potential and become confident, capable, and compassionate individuals.
+            </p>
+            <hr>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" src="caro2.jpg" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title" style="color: rgb(183, 0, 31);"> <a href="">Donate clothing and other items"</a></h5>
+            <p class="card-text text-muted">
+
+              Our children deserve to feel proud and confident in their clothing. Your clothing
+              donations will help us provide them with comfortable and dignified attire,
+              suitable for everyday wear and special occasions.
+            </p>
+            <hr>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <!--car1 end-->
+  <!--gallery-->
+
+  <main class="mt-5">
+    <div class="container">
+      <!--section head start-->
+      <h2 class="h2-responsive section-heading fw-bold text-center my-2" style="color: rgb(183, 0, 31);">
+        Gallery
+      </h2>
+      <p class="text-center w-responsive mx-auto mb-1">
+        <strong> Experience the joy and resilience of our children! Visit our gallery</strong>
+        ..see the smiles and stories of our children in our gallery!
+      </p>
+      <!--section head ends-->
+      <!-- Gallery -->
+      <div class="row mt-5">
+        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+          <img src="chi4.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+
+          <img src="chi2.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Wintry Mountain Landscape" />
+        </div>
+
+        <div class="col-lg-4 mb-4 mb-lg-0">
+          <img src="chi1.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Mountains in the Clouds" />
+
+          <img src="chi3.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+        </div>
+
+        <div class="col-lg-4 mb-4 mb-lg-0">
+          <img src="chi5.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Waves at Sea" />
+
+          <img src="chi6.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Yosemite National Park" />
+        </div>
+      </div>
+      <!-- Gallery -->
+    </div>
+  </main>
+
+
+  <!-- Footer -->
+
+
+  <?php include('footer.php'); ?>
+  <!-- Footer -->
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+  <script>
+    $('.carousel').carousel();
+    var myCarousel = document.querySelector('#myCarousel')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 2000,
+      wrap: false
+    })
+  </script>
+</body>
+
+</html>
